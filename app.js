c6d9ae78e8768a104c7913af2de64ed97d14da0f
piyushSharma97 <piyushsharma70141@gmail.com>
2020-12-14 11:46:03 +0530

    globalDB

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 var localDB="mongodb://localhost:27017/Employee"
 var globalDB ='mongodb+srv://piyush5sarvika:sarvika5456@cluster0.4yvdu.mongodb.net/employee_crud?retryWrites=true&w=majority'
-mongoose.connect(localDB, {  useNewUrlParser: true,
+mongoose.connect(globalDB, {  useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true});
